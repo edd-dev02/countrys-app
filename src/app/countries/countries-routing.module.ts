b5b665903eed38f498fed293980fbc1a75489553
Eduardo Sandoval <edsandoval.dev@gmail.com>
2026-01-22 21:03:21 -0600

    Declaración de rutas básicas en modulo countries

--- src/app/countries/countries-routing.module.ts
+++ src/app/countries/countries-routing.module.ts
@@ -1,8 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes } from '@angular/router';
+import { ByCapitalPageComponent } from './pages/by-capital-page/by-capital-page.component';
+import { ByCountryPageComponent } from './pages/by-country-page/by-country-page.component';
 
 const routes: Routes = [
+  {
+    path: "by-capital",
+    component: ByCapitalPageComponent
+  },
+  {
+    path: "by-country",
+    component: ByCountryPageComponent
+  },
+  {
 
+  },
+  {
+
+  },
 ];
 
 
