87f2a8de2d0428a1d96ee7506680e17dd8403d9a
Eduardo Sandoval <edsandoval.dev@gmail.com>
2026-01-22 21:06:58 -0600

    Añadir ruta que recibe parametro y finalizacion de configuración del routing

--- src/app/countries/countries-routing.module.ts
+++ src/app/countries/countries-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
-import { Routes } from '@angular/router';
+import { Router, RouterModule, Routes } from '@angular/router';
 import { ByCapitalPageComponent } from './pages/by-capital-page/by-capital-page.component';
 import { ByCountryPageComponent } from './pages/by-country-page/by-country-page.component';
+import { ByRegionPageComponent } from './pages/by-region-page/by-region-page.component';
+import { CountryPageComponent } from './pages/country-page/country-page.component';
 
 const routes: Routes = [
   {
@@ -13,16 +15,21 @@ const routes: Routes = [
     component: ByCountryPageComponent
   },
   {
-
+    path: "by-region",
+    component: ByRegionPageComponent
   },
   {
-
+    path: "by/:id",
+    component: CountryPageComponent
   },
 ];
 
-
 @NgModule({
-  imports: [],
-  exports: [],
+  imports: [
+    RouterModule.forChild(routes)
+  ],
+  exports: [
+    RouterModule
+  ],
 })
 export class CountriesRoutingModule { }
