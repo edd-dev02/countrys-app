b2315678e96ab789ded3f8532f369f9f4fa4e58f
Eduardo Sandoval <edsandoval.dev@gmail.com>
2026-01-22 21:22:51 -0600

    Se añaden el router hijo al router principal y se implementan las rutas de los componentes de countries

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -18,6 +18,10 @@ const routes: Routes = [
     component: ContactPageComponent
   },
   {
+    path: "countries",
+    loadChildren: () => import("./countries/countries.module").then(m => m.CountriesModule) // Carga perezosa del módulo
+  },
+  {
     path: "**",
     redirectTo: "home"
   },
