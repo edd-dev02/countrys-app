822a8657012a30b8584fdfa72c111aa33b12baa1
Eduardo Sandoval <edsandoval.dev@gmail.com>
2026-01-24 16:13:52 -0600

    Http: creación del servicio para hacer peticiones e importación del módulo http en el módulo principal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { SharedModule } from './shared/shared.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -13,7 +14,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    SharedModule
+    SharedModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
