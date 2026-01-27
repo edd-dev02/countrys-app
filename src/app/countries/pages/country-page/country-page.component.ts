acdcfc7f7ca1d4757deae21902326f685cad3d57
Eduardo Sandoval <edsandoval.dev@gmail.com>
2026-01-26 21:54:46 -0600

    Pagina detalle: Obtenemos la información de un país usando si ID en una URL

--- src/app/countries/pages/country-page/country-page.component.ts
+++ src/app/countries/pages/country-page/country-page.component.ts
@@ -1,10 +1,59 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { CountriesService } from '../../services/countries.service';
+import { switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-country-page',
   templateUrl: './country-page.component.html',
   styles: ``
 })
-export class CountryPageComponent {
+export class CountryPageComponent implements OnInit {
+
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private countriesService: CountriesService,
+    private router: Router,
+  ) { }
+
+  // Manera limpia de hacerlo: Uso de RxJs
+
+  ngOnInit(): void {
+    this.activatedRoute.params
+      .pipe(
+        switchMap( ({id}: Params) =>  this.countriesService.searchCountryByAlphaCode( id )),
+      )
+      .subscribe( country => {
+
+        if(!country)  {
+          return this.router.navigateByUrl("");
+        }
+
+        console.log("Tenemos país");
+        return;
+
+      })
+  }
+
+
+  /*
+
+  Manera práctica de hacerlo
+
+  ngOnInit(): void {
+    this.activatedRoute.params
+      .subscribe(({ id }: Params) => {
+        this.searchCountry( id );
+      })
+  }
+
+  public searchCountry(code: string) {
+    this.countriesService.searchCountryByAlphaCode(code)
+      .subscribe(country => {
+        console.log({ country });
+      })
+  }
+  */
+
 
 }
--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { catchError, Observable, of, tap } from 'rxjs';
+import { catchError, map, Observable, of, tap } from 'rxjs';
 import { Country } from '../interfaces/country.interface';
 
 @Injectable({ providedIn: 'root' })
@@ -9,6 +9,17 @@ export class CountriesService {
   private baseUrl: string = "https://restcountries.com/v3.1";
 
   constructor(private httpClient: HttpClient) { }
+
+  public searchCountryByAlphaCode(code: string): Observable<Country | null> {
+    const url = `${this.baseUrl}/alpha/${code}`;
+
+    return this.httpClient.get<Country[]>(url)
+      .pipe(
+        map( countries => countries.length > 0 ? countries[0] : null),
+        catchError(() => of (null))
+
+      )
+  }
 
   public searchCapital(term: string): Observable<Country[]> {
 
